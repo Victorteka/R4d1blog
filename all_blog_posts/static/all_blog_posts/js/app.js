c1269b9c0180ddab607e06c071e10b37aca7fb4b
Victor Teka <ambukateka@gmail.com>
2020-07-12 11:35:53 +0300

    Add function to fetch news from newsApi

---
+++ all_blog_posts/static/all_blog_posts/js/app.js
@@ -0,0 +1,1 @@
+document.getElementById("hello").addEventListener("click", alert("Hello"));
--- all_blog_posts/views.py
+++ all_blog_posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
+import requests
 
 from .models import Post
 
@@ -8,9 +9,20 @@ from .models import Post
 
 def all_blog_posts(request):
     posts = Post.objects.all()
-    return render(request, 'all_blog_posts.html', {"posts": posts})
+    news = get_news()
+    return render(request, 'all_blog_posts.html', {"posts": posts, "news": news})
 
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+# get cyber security articles
+
+
+def get_news():
+    url = 'https://newsapi.org/v2/everything?q=cyber%20security&apiKey=14ae627890954195922391f40adfdf58'
+    r = requests.get(url)
+    news = r.json()
+    news_list = news['articles']
+    return news_list
